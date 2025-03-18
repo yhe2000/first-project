96c75dbce5c3e90ea35bbb4beb008f2bd274336f
Yara <yaraadwan155@gmail.com>
2025-03-18 04:23:37 +0300

    fifth project

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,35 +3,46 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
+use App\Models\Task;
 class TaskController extends Controller
 {
    public function index(){
-    $tasks = DB::table('tasks')->get();
+    //DB::table('tasks')->get();
+    $tasks = Task::all();
     return view('tasks', compact(var_name:'tasks'));
    }
 
    public function create(){
     $task_name = $_POST['name'];
-    DB::table(table: 'tasks')->insert(['name'=> $task_name]);
+   // DB::table(table: 'tasks')->insert(['name'=> $task_name]);
+    $task = new Task;
+    $task->name = $task_name;
+    $task->save();
     return redirect()->back();
    }
 
    public function destroy($id){
-    DB::table('tasks')->where('id',$id)->delete();
+   // DB::table('tasks')->where('id',$id)->delete();
+   $task = Task::findOrfail($id);
+   $task->delete();
     return redirect()->back();
    }
 
    public function edit($id){
-    $task = DB::table('tasks')->where('id',$id)->first();
-    $tasks = DB::table('tasks')->get();
+    //$task = DB::table('tasks')->where('id',$id)->first();
+    //$tasks = DB::table('tasks')->get();
+    $task = Task::findOrFail($id);
+    $tasks = Task::all();
     return view('tasks',compact('task','tasks'));
 
    }
 
-   public function update(){
+   public function update(Request $request){
     $id = $_POST['id'];
-    DB::table('tasks')->where('id','=',$id)->update(['name'=>$_POST['name']]);
+   // DB::table('tasks')->where('id','=',$id)->update(['name'=>$_POST['name']]);
+   $task = Task::findOrFail($id);
+   $task->name = $request->name;
+   $task->save();
     return redirect('tasks');
    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,26 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = DB::table('users')->get();
+       // $users = DB::table('users')->get();
+        $users = User::all();
         return view('users', compact('users'));
     }
 
     // إنشاء مستخدم جديد
-    public function create()
+    public function create(Request $request)
     {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $password = Hash::make($_POST['password']);
 
-        DB::table('users')->insert([
+      /*  DB::table('users')->insert([
             'name' => $name,
             'email' => $email,
             'password' => $password
+        ]); */
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
 
         return redirect()->back();
@@ -32,14 +41,18 @@ class UserController extends Controller
 
     public function destroy($id)
     {
-        DB::table('users')->where('id', $id)->delete();
-        return redirect()->back();
+       // DB::table('users')->where('id', $id)->delete();
+       $user = User::findOrFail($id); // العثور على المستخدم أو إرجاع خطأ 404
+       $user->delete();
+       return redirect()->back();
     }
 
     public function edit($id)
     {
-        $user = DB::table('users')->where('id', $id)->first();
-        $users = DB::table('users')->get();
+       // $user = DB::table('users')->where('id', $id)->first();
+       // $users = DB::table('users')->get();
+        $user = User::findOrFail($id);
+        $users = User::all();
         return view('users', compact('user', 'users'));
     }
 
